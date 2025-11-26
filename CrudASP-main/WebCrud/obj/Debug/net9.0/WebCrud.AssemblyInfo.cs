c872726f6fcb277863617d77e585953b30d36c8d
jamaral33 <jpgamede07@gmail.com>
2025-11-26 10:36:42 -0300

    Inserindo imagens e fazendo mudanças no background

--- CrudASP-main/WebCrud/obj/Debug/net9.0/WebCrud.AssemblyInfo.cs
+++ CrudASP-main/WebCrud/obj/Debug/net9.0/WebCrud.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebCrud")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+beac8a787ba91f1466212afb3275ae703b7ad28f")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebCrud")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebCrud")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
